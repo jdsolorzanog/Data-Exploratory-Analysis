fd8089cc391b7544f84cd9bd36338badb46a6e1f
jdsolorzanog <71285652+jdsolorzanog@users.noreply.github.com>
2020-12-18 09:42:14 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Generar subconjunto de datos
+baltimore <- NEI[NEI$fips=="24510", ]
+BYTE <- aggregate(Emissions ~ year+ type, baltimore, sum)
+
+# Generar gráfico
+graphic <- ggplot(BYTE, aes(year, Emissions, color = type))
+graphic <- graphic + geom_line() + xlab("year") + ylab("Total Emissions") + ggtitle("Total Emmisions by type in Baltimore")
+print(graphic)
